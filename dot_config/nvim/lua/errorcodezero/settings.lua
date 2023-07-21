92e2e6ef707abb4e33fb045cf16e51657b23ef60
ErrorCode0 <noreply@errorcodezero.ml>
2023-07-21 10:11:06 -0700

    Changing nvim settings

--- dot_config/nvim/lua/errorcodezero/settings.lua
+++ dot_config/nvim/lua/errorcodezero/settings.lua
@@ -4,3 +4,27 @@ vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 vim.opt.termguicolors = true
 vim.cmd.colorscheme "catppuccin-mocha"
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+
+vim.opt.smartindent = true
+
+vim.opt.wrap = false
+
+vim.opt.swapfile = false
+vim.opt.backup = false
+
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+
+vim.opt.termguicolors = true
+
+vim.opt.scrolloff = 8
+vim.opt.signcolumn = "yes"
+vim.opt.isfname:append("@-@")
+
+vim.opt.updatetime = 50
+
+vim.opt.colorcolumn = "80"
