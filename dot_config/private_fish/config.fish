09d64a5520bfcc4ce22f5e9107e7d51639af8b2e
ErrorCode0 <me@errorcodezero.dev>
2024-03-14 18:37:18 -0700

    Adding fish config that adds powerline by r58

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -4,6 +4,11 @@ if status is-interactive
 	alias t="tmux"
     alias s="kitty +kitten ssh"
     alias lt="nvim leetcode.nvim"
+    alias ls="lsd -l"
+    alias fr="grep -rl $OLDTEXT . | xargs sed -i 's/$OLDTEXT/$NEWTEXT/g'"
+    function fish_prompt
+        powerline $pipestatus
+    end
 end
 
 fish_add_path /home/errorcodezero/.spicetify
