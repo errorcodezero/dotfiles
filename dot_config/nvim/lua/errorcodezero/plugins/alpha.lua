7aeffc163763b9ef2af39149029ad06a01eff44f
ErrorCode0 <me@errorcodezero.dev>
2024-10-02 00:01:15 -0700

    Added the new nvim startup screen

--- dot_config/nvim/lua/errorcodezero/plugins/alpha.lua
+++ dot_config/nvim/lua/errorcodezero/plugins/alpha.lua
@@ -12,7 +12,11 @@ return {
             "╚══════╝╚═╝░░╚═╝╚═╝░░╚═╝░╚════╝░╚═╝░░╚═╝░╚════╝░░╚════╝░╚═════╝░╚══════╝░╚════╝░",
         }
 	dashboard.section.buttons.val = {
-	   dashboard.button("1", "Open File", ":Telescope find_files")
+	   dashboard.button("1", "󰈔 Open File", ":Telescope find_files<enter>"),
+	   dashboard.button("2", "󰨭 Live Grep", ":Telescope live_grep<enter>"),
+	   dashboard.button("3", "󱊈 Plugins", ":Lazy<enter>"),
+	   dashboard.button("4", "󰒋 LSP Servers", ":Mason<enter>"),
+	   dashboard.button("5", "󰅖 Quit", ":q!<enter>")
 	}
         require'alpha'.setup(dashboard.config)
     end
